b5a401d7709d1463f9a24e92660091efed0058ff
WellJons <kromovnikita@gmail.com>
2026-08-21 18:00:44 +0700

    Add macOS privacy bridge header

---
+++ cmd/linkvideo-monitor/privacy_darwin.h
@@ -0,0 +1,36 @@
+#ifndef LINKVIDEO_PRIVACY_DARWIN_H
+#define LINKVIDEO_PRIVACY_DARWIN_H
+
+#include <stdint.h>
+
+typedef struct {
+    uintptr_t handle;
+    int valid;
+    int secure;
+    int editable;
+    int enabled;
+    int focused;
+    int offscreen;
+    double x;
+    double y;
+    double width;
+    double height;
+    char *name;
+    char *identifier;
+    char *role;
+    char *subrole;
+    char *help;
+    char *process;
+    char *window_title;
+    char *dom_class;
+    char *aria_role;
+    char *aria_props;
+} LVPrivacySample;
+
+int lv_privacy_is_trusted(int prompt);
+int lv_privacy_copy_focused(LVPrivacySample *out);
+int lv_privacy_refresh(uintptr_t handle, LVPrivacySample *out);
+void lv_privacy_release(uintptr_t handle);
+void lv_privacy_free_sample(LVPrivacySample *sample);
+
+#endif
