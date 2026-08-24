574547757d279e2b046766bc1a47bab93d4cb977
WellJons <kromovnikita@gmail.com>
2026-08-24 11:00:47 +0700

    Format Windows updater regression test

--- cmd/linkvideo-monitor/update_asset_windows_test.go
+++ cmd/linkvideo-monitor/update_asset_windows_test.go
@@ -6,8 +6,8 @@ import "testing"
 
 func TestWindowsAutomaticUpdateAssetNameRemainsSetupEXE(t *testing.T) {
 	cases := map[string]string{
-		"0.8.13":        "linkvideo.monitor_0.8.13_setup.exe",
-		"v0.8.13-beta":  "linkvideo.monitor_0.8.13_setup.exe",
+		"0.8.13":         "linkvideo.monitor_0.8.13_setup.exe",
+		"v0.8.13-beta":   "linkvideo.monitor_0.8.13_setup.exe",
 		"1.0.0+build.42": "linkvideo.monitor_1.0.0_setup.exe",
 	}
 	for version, want := range cases {
